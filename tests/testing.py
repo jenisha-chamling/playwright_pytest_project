97e6e1d46e5eedc030d710bcf48b1fd2dc20513f
Jenisha Rai <jenisha.rai.2224@gmail.com>
2026-07-12 04:54:04 +0545

    Removed a file

--- tests/testing.py
+++
@@ -1,5 +0,0 @@
-from playwright.sync_api import Page, expect
-def test_verifyUrl(page:Page):
-  page.goto("http://localhost/php_practice/index.php")
-  expect(page).to_have_url("http://localhost/php_practice/index.php")
-  
