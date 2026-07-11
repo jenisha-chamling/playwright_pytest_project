b3b209a3a3dcbbe4bebcf0e82767c7687a491eb8
Jenisha Rai <jenisha.rai.2224@gmail.com>
2026-07-12 04:48:49 +0545

    Addition of elements in index.php

--- index.php
+++ index.php
@@ -14,23 +14,38 @@
 <body>
   <form action="index.php" method="post">
     <h2> Student Enrollment Registration </h2>
-    First Name:
-    <input type="text" name="first" required>
-    Last Name:
-    <input type="text" name="last" required> <br>
+    <label for="fname">First Name</label>
+    <input type="text" id= "fname" name="first" >
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="last" > <br>
     <br>
     
-    Grade:
-    <input type="text" name="grade" required>
+    <label for="grade">Grade</label>
+    <input type="text" id="grade" name="grade" >
     <br>
     <br>
     
 
-    RollNo.:
-    <input type="number" name="roll" min="1" required>
+    <label for="rolln">Roll No.</label>
+    <input type="number" id="rolln" name="roll" min="1" >
     <br>
     <br>
-    <input type="submit" name="submit" value="register"> 
+
+<label>Gender:</label><br>
+
+<input type="radio" id="male" name="gender" value="Male">
+<label for="male">Male</label>
+
+<input type="radio" id="female" name="gender" value="Female">
+<label for="female">Female</label>
+
+<input type="radio" id="other" name="gender" value="Other">
+<label for="other">Other</label>
+    <br>
+    <br>
+
+    <input type="submit" name="submit" value="Register">
 
 
   </form>
@@ -48,7 +63,8 @@
     empty($_POST["first"]) ||
     empty($_POST["last"]) ||
     empty($_POST["grade"]) ||
-    empty($_POST["roll"])
+    empty($_POST["roll"]) ||
+    empty($_POST["gender"])
 ) {
 
     if(empty($_POST["first"]))
@@ -62,6 +78,9 @@
 
     if(empty($_POST["roll"]))
         echo "Enter your roll number<br>";
+    
+    if(empty($_POST["gender"]))
+      echo "Select your gender<br>";
 
 }
 elseif ($_POST["roll"] <= 0) {
@@ -75,9 +94,10 @@ else {
     $last = $_POST["last"];
     $grade = $_POST["grade"];
     $rollNo = $_POST["roll"];
+    $gender = $_POST["gender"];
 
-    $sql = "INSERT INTO information (FirstName, LastName, Grade, RollNo)
-            VALUES ('$first', '$last', '$grade', '$rollNo')";
+    $sql = "INSERT INTO information (FirstName, LastName, Grade, RollNo, Gender)
+            VALUES ('$first', '$last', '$grade', '$rollNo', '$gender')";
 
     if (mysqli_query($conn, $sql)) {
     header("Location: index.php?success=1");
